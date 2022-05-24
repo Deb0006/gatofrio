0372a99f8807dc21cdc7fb484ffe9ee27ef64f6c
Deb0006 <34969423+Deb0006@users.noreply.github.com>
2022-05-24 09:06:28 -0400

    modal integration
    modal integration for mobile

---
+++ menuData.js
@@ -0,0 +1,31 @@
+export default {
+  "Gato Frio":
+    "Bizcochuelo, 1 dulchicha de helado, 3 toppings, 3 salsas y cereza.",
+  GatiBurguer:
+    "Donut con queso fundido, crema chantilly, 1 heliburguer, 2 toppings, 2 salsas y  azucar pulverizada. ",
+  GatoTaco:
+    "Crepe con crema chantilly o crema de mani,  1 dulchicha de helado, 2 toppings, 2 salsas, rodajitas de banano, chocolate rayado y azucar pulverizada.",
+  Mojigato:
+    "Bizcochuelo rayado banado en crema de whiskey o leche condensada, 2 toppings, 1 helado, barquillo salsa y galleta oreo.",
+  GatiPizza:
+    "Crepe con queso fundido, 2 dulchichas picadas, 3 toppings y 3 salsas.",
+  Brownie: "Brownie caliente con helado, barquillo, 1 salsa y cereza.",
+  Malteada: "Helado con 1 salsa, barquillo y cereza.",
+  Banana: "Banano con 3 sabores de helado, 1 salsa, barquillo y cerezas.",
+};
+
+// SATO FRIO Bizcochuelo. una dulchicha del sabor de helado que elijas. 3 toppings. 3 salsas a elección y cereza.
+// CATIMURGUER
+// Dona can queso fundido. heliburguer , ,,,,, del sabor de helado que elijas, -- '- Li crema chantilly, 2 toPPings, 2 salsas a  d elección y azocar pulverizada. -...."' „ ,, S 8.500
+// GATOTACO
+// Crepe con crema de mani o crema chantilly, una dulchicha picada del sabor de helado que elijas, rodillitas de banano. 2 toPPings. 2 salsas. chocolate rayado y azocar pulverizada. 59.000 GATIP1714 Crepe con queso fundido. 2 dulchichas picadas del sabor de helado que elijas, 3 toppings. 3 salsas a elección.
+// S 12.500 Puedes añadir un TOPF'ING a tu postre por un valor adicional de 81.000 y si deseas acompañarlo con AGUA o CAFÉ por solo S 1.500
+
+// MOJIGATO Bizcochuelo rayado bañado en crema de whisky o leche condensada. 2 toppings. tu helado favorito. barquillo. salsa y galleta oreo. $ 8.500
+// Brownie caliente con tu helado favorito barquillo. 1 salsa a elección y cereza. 7.500
+// PANANA
+// B
+// //ir
+// Banano. 3 sabores de tu helado favorito barquillo. 1 salsa a elección y cerezas. $ 8.000
+// d MALTEADA aje el sabor de tu helado favorito. 1 salsa elección. barquillo y cereza $ 7.800
+// Puedes añadir un TOPPING a tu postre por un valor adicional de $1.000 y si deseas acompañarlo con AGUA o CAFÉ Por solo $ 1.500
---
+++ script.js
@@ -0,0 +1,77 @@
+import menuData from "/menuData.js";
+
+const menuObj = menuData;
+const imgButton = [...document.querySelectorAll(".menu__container")];
+const modal = document.querySelector(".popup-images");
+const button = document.querySelector(".container__button");
+const overlay = document.querySelector("#overlay");
+
+const imgProduct = document.querySelector(".container__img--image");
+const nameProduct = document.querySelector(".container__text--name");
+const descProduct = document.querySelector(".container__text--description");
+function loadModal(itemMenu) {
+  const img = itemMenu.querySelector(".menu__container--images").src;
+  const name = itemMenu.querySelector(".menu__container--name").innerText;
+  imgProduct.src = img;
+  nameProduct.innerText = name;
+  descProduct.innerText = menuObj[name];
+}
+
+imgButton.forEach((img) => {
+  img.addEventListener("click", () => {
+    loadModal(img);
+    modal.classList.add("active");
+    modal.classList.remove("inactive");
+  });
+});
+
+button.addEventListener("click", () => {
+  modal.classList.remove("active");
+  // modal.style.animation = "600ms opacityAnimationIn ease-out";
+  modal.classList.add("inactive");
+});
+overlay.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+// const imagenX = document.querySelector(".menu__containers")
+
+// const openModalButtons = document.querySelectorAll("[data-modal-target]");
+// const closeModalButtons = document.querySelectorAll("[data-close-button]");
+// const overlay = document.getElementById("overlay");
+// console.log(openModalButtons);
+// console.log(closeModalButtons);
+
+// openModalButtons.forEach((div) => {
+//   div.addEventListener("click", () => {
+//     const modal = document.querySelector(div.dataset.modalTarget);
+//     console.log(div);
+//     console.log(modal);
+//     openModal(modal);
+//   });
+// });
+// overlay.addEventListener("click", () => {
+//   const modal = document.querySelectorAll(".popup-images.active");
+//   modal.forEach((modal) => {
+//     closeModal(modal);
+//   });
+// });
+// closeModalButtons.forEach((div) => {
+//   div.addEventListener("click", () => {
+//     const modal = div.closest(".popup-images");
+//     console.log(div);
+//     console.log(modal);
+//     closeModal(modal);
+//   });
+// });
+
+// function openModal(modal) {
+//   if (modal == null) return;
+//   modal.classList.add("active");
+//   overlay.classList.add("active");
+// }
+// function closeModal(modal) {
+//   if (modal == null) return;
+//   modal.classList.remove("active");
+//   overlay.classList.remove("active");
+// }
